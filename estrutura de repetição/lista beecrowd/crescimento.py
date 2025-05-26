eb07bd4918932453520cb4b3e7752fd70c961eea
IsabelMelo4 <isabelmelo630@gmail.com>
2025-05-26 19:59:56 -0300

    fiz mais exercicios

---
+++ estrutura de repetição/lista beecrowd/crescimento.py
@@ -0,0 +1,11 @@
+testes = int(input())
+
+for i in range(1, testes+1):
+    populacaoA,  populacaoB, crescimento1,  crescimento2  = input().split()
+    crescimento1,  crescimento2 = float
+    populacaoA,  populacaoB = int
+
+    while populacaoA < populacaoB:
+        resA = populacaoA + (populacaoA * crescimento1 / 100)
+        resB = populacaoB + (populacaoB * crescimento2 / 100)
+        
--- estrutura de repetição/lista beecrowd/soma de pares.py
+++ estrutura de repetição/lista beecrowd/soma de pares.py
@@ -2,8 +2,8 @@
 x = int(input())
 
 while x != 0:
+    soma = 0
     for i in range(x, x+10):
-        soma = 0
         if i %2 == 0:
             soma += i 
     print(soma)
