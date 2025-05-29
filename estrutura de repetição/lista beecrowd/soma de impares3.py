a9fea0385c53845c0277ec72e24ed198f3f8a584
IsabelMelo4 <isabelmelo630@gmail.com>
2025-05-29 09:07:58 -0300

    SOMA DE IMPARES 3

--- estrutura de repetição/lista beecrowd/soma de impares3.py
+++ estrutura de repetição/lista beecrowd/soma de impares3.py
@@ -1,6 +1,16 @@
 n = int(input())
 for i in range(n):
-    x, y = int(input().split())
+    x, y = map(int, input().split())
+    
+    somador = 0
+    contador  = 0
 
-    if x < y :
-     x, y = y, x
+
+    while contador < y:
+        if x % 2 != 0:
+            somador += x
+            contador += 1
+        x += 1
+
+    
+    print(somador)
