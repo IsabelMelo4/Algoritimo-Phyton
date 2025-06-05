71a9606f17a6be4fffb0c9cb74250df5f34b4cd8
IsabelMelo4 <isabelmelo630@gmail.com>
2025-06-05 07:21:46 -0300

    commit

--- extra/tipos de triangulo.py
+++ extra/tipos de triangulo.py
@@ -1,19 +1,20 @@
 a, b, c = map(float, input().split())
 
-if a > (b + c):
-     print("NAO FORMA TRIANGULO ")
+if (b + c) > a:
+     print("NAO FORMA TRIANGULO")
 
-if a ** 2 == b**2 + c **2:
+else:  
+    if a ** 2 == b**2 + c **2:
      print("TRIANGULO RETANGULO")
 
-if a ** 2 > b**2 + c **2:
+    if  b**2 + c **2 > a ** 2 :
      print("TRIANGULO OBTUSANGULO")
 
-if a ** 2 < b**2 + c **2:
+    if  b**2 + c **2 < a **2:
      print("TRIANGULO ACUTANGULO")
 
-if a == b and b == c:
+    if a == b and b == c:
      print("TRIANGULO EQUILATERO")
     
-if a != b or b != c or c != a:
+    if a != b or b != c or c != a:
      print("TRIANGULO ISOSCELES ")
