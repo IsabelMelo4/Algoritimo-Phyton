b17f15f8a9567bcac7256f14bb3bef66e1f18c34
Isabel Melo <109425080+IsabelMelo4@users.noreply.github.com>
2025-06-12 09:09:15 -0300

    Update sequencialogica2.py

--- estrutura de repetição/lista beecrowd/lista7/sequencialogica2.py
+++ estrutura de repetição/lista beecrowd/lista7/sequencialogica2.py
@@ -1,9 +1,12 @@
-x, y = map(int, input().split())
-cont = 0 
-for i in range(1, y+1):
+x, y = map(int, input().split()) #ele vai ler dois numeros o tamanho da linha e a quantidade
+cont = 0 #contador para controlar a linha 
+
+for i in range(1, y+1): #vai contar de 1 ate y 
+       
        print(i, end=" ") #innves de quebrar linha ele coloca um espaço dai ele 
                          #imprime tudo na mesma linha 
-       cont += 1
-       if cont == x:
-         print(end="\n")   
-         cont=0
+       cont += 1 #conntador vai somando 1 
+         
+       if cont == x: #se caso ele fique do tamanho da linha que é x 
+         print(end="\n")  #ele quebra pra proxima linha 
+         cont=0 # e zera o contador 
