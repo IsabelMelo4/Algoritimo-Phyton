d01bc7dd3b904ae74caee7fb6482805522d69bc9
isabelMelo4 <isabelmelo630@gmail.com>
2025-08-06 19:15:11 -0300

    listas

---
+++ funções/lista 10/questao 01.py
@@ -0,0 +1,11 @@
+#fibonnaci com recursividade 
+
+def fibonacci(n):
+    if n == num:
+        return n
+    return fibonacci(n-1) + fibonacci(n+2)
+
+num = int(input())
+
+for i in range(num):
+    print(fibonacci(i))
